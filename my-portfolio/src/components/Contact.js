162c8dc0b2db49e4d31c26b23ac8064e64ce0ae9
Chitsanupong <first30952@gmail.com>
2021-05-26 21:58:13 +0700

    contact form

--- my-portfolio/src/components/Contact.js
+++ my-portfolio/src/components/Contact.js
@@ -1,9 +1,73 @@
-import React from 'react'
+import React from "react"
+
+import { MdEmail, MdLocationOn } from "react-icons/md"
+import {
+  AiFillFacebook,
+  AiFillInstagram,
+  AiFillGithub,
+  AiFillLinkedin,
+  AiFillMediumSquare,
+} from "react-icons/ai"
+import { BiPhoneCall } from "react-icons/bi"
 
 export default function Contact() {
-    return (
-        <div className="Contact">
-           <h2 className="Contact__header">Contact</h2> 
+  return (
+    <div className="Contact">
+      <h2 className="Contact__header">Contact</h2>
+      <div className="Contact__container">
+        <div className="Contact__social">
+          <div className="Contact__social-container">
+            <MdEmail className="Contact__icon" />
+            <p>c.tangvasinkul@gmail.com</p>
+          </div>
+          <div className="Contact__social-container">
+            <BiPhoneCall className="Contact__icon" />
+            <p>085-939-2573</p>
+          </div>
+          <div className="Contact__social-container">
+            <MdLocationOn className="Contact__icon" />
+            <p>Samut Prakan, Thailand</p>
+          </div>
+          <div className="Contact__social-container">
+            <AiFillFacebook className="Contact__icon" />
+            <p>Chitsanupong Tangvasinkul</p>
+          </div>
+          <div className="Contact__social-container">
+            <AiFillInstagram className="Contact__icon" />
+            <p>first.neverrest</p>
+          </div>
+          <div className="Contact__social-container">
+            <AiFillLinkedin className="Contact__icon" />
+            <p>Chitsanupong Tangvasinkul</p>
+          </div>
+          <div className="Contact__social-container">
+            <AiFillMediumSquare className="Contact__icon" />
+            <p>Neverrest</p>
+          </div>
+          <div className="Contact__social-container">
+            <AiFillGithub className="Contact__icon" />
+            <p>firstneverrest</p>
+          </div>
         </div>
-    )
+
+        <form className="Contact__form">
+          <h4 className="Contact__form-header">Contact me by email</h4>
+          <div className="Contact__form-container">
+            <div className="Contact__form-fullName">
+              <label htmlFor="fullName">Full Name</label>
+              <input type="text" id="fullName" />
+            </div>
+            <div className="Contact__form-email">
+              <label htmlFor="email">E-mail</label>
+              <input type="email" id="email" />
+            </div>
+          </div>
+
+          <label htmlFor="message">Message</label>
+          <textarea type="text" id="message" />
+          <button className="Contact__form-button">Send Message</button>
+        </form>
+      </div>
+    </div>
+  )
 }
--- my-portfolio/src/layouts/Footer.js
+++ my-portfolio/src/layouts/Footer.js
@@ -1,5 +1,13 @@
 import React from "react"
 
+import {
+  AiFillFacebook,
+  AiFillInstagram,
+  AiFillGithub,
+  AiFillLinkedin,
+  AiFillMediumSquare,
+} from "react-icons/ai"
+
 export default function Footer() {
   return (
     <div className="Footer">
@@ -9,11 +17,21 @@ export default function Footer() {
           Copyright © 2021 Chitsanupong Tangvasinkul. All rights reserved.
         </p>
         <div className="Footer__social">
-          <a href="#" className="Footer__social-ig"></a>
-          <a href="#" className="Footer__social-facebook"></a>
-          <a href="#" className="Footer__social-github"></a>
-          <a href="#" className="Footer__social-linkedin"></a>
-          <a href="#" className="Footer__social-medium"></a>
+          <a href="https://www.facebook.com/firstnasa/" target="_blank" className="Footer__social-facebook">
+            <AiFillFacebook />
+          </a>
+          <a href="https://www.instagram.com/first.neverrest/" target="_blank" className="Footer__social-ig">
+            <AiFillInstagram />
+          </a>
+          <a href="https://github.com/firstneverrest" target="_blank" className="Footer__social-github">
+            <AiFillGithub />
+          </a>
+          <a href="https://www.linkedin.com/in/chitsanupong-tangvasinkul-934648202/" target="_blank" className="Footer__social-linkedin">
+            <AiFillLinkedin />
+          </a>
+          <a href="https://neverrest.medium.com/" target="_blank" className="Footer__social-medium">
+            <AiFillMediumSquare />
+          </a>
         </div>
       </div>
     </div>
